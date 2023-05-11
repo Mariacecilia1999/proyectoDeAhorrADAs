4207a4fba3e0018a426339965956ca4cbead676a
Mariacecilia1999 <conticecilia199@gamil.com>
2023-05-11 06:23:10 -0300

    Agrego funciones flechas para reutilizar de selector, mostrar y ocultar

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+
+/***************FUNCIONES REUTILIZABLES*************/
+const $ = selector => document.querySelector(selector)
+const mostrar = selector => $(selector).classList.remove('hidden')
+const ocultar = selector => $(selector).classList.add('hidden')
+
+
+
+/**************FUNCIONALIDAD DEL NAVBAR*************/
+
+
+
+
+
+
+/************************************************/
+
+
+
+
+
+
+
+/***************INICIALIZACIÓN DE FUNCIONES***********/
+
+
+
+
+/*************************************************/
